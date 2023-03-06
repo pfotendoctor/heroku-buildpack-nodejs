b033d860f1cef52c2971ef8e6ea6e31dadebf933
tillpd <till.wierbach@tierarztpluspartner.de>
2023-03-06 18:34:19 +0100

    test

---
+++ lib/turbo.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+install_turbo {
+  monitor "install turbo" "npm install --global turbo"
+
+}
+
+prune {
+  monitor "turbo prune" turbo prune --scope==tp-backend
+}
+
+install_turbo
+prune
