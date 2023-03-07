7334aa8b0d44d7ec1519d95d1391490e0b3a2548
tillpd <till.wierbach@tierarztpluspartner.de>
2023-03-07 12:36:03 +0100

    test

--- lib/turbo.sh
+++ lib/turbo.sh
@@ -1,12 +1,1 @@
 #!/usr/bin/env bash
-
-install_turbo() {
-  monitor "install turbo" "yarn add turbo -W"
-}
-
-prune() {
-  monitor "turbo prune" "yarn turbo prune --scope==tp-backend"
-}
-
-install_turbo
-prune
